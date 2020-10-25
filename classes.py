0ac7b48f30431f7a4836800a82ad0b00bd2dacfa
ccastro-pedro <70175236+ccastro-pedro@users.noreply.github.com>
2020-10-25 13:32:21 -0300

    Add files via upload
    Mudança feita no método __get_files

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 """
 Classe utilizada para realização do problema
+
 Words.content = {'nome do arquivo 1': {'texto':[], 'Vetor N únicos': [], 'Vetor N TwoGram': []},
                 'nome do arquivo 2': {'texto':[], 'Vetor N únicos': [], 'Vetor N TwoGram': []}, ...,
                 'nome do arquivo N': {'texto':[], 'Vetor N únicos': [], 'Vetor N TwoGram': []},
@@ -10,6 +11,7 @@ import re
 import os
 
 STOP_WORDS = ['o', 'a', 'que', 'me', 'na', 'em', 'no', 'para', 'de', 'da', 'do', 'das', 'dos', 'nas', 'nos']
+
 
 def add_element(d, key, value):
     if key not in d:
@@ -55,10 +57,10 @@ class Words:
             with open(file, 'r', encoding="utf8") as f:
                 name = os.path.basename(f.name)
                 text = f.read()
-                text = re.findall(r"[\w']+", text)  # (?<![0-9])
-                text = [i.lower() for i in text]
-                text_sem_stop_words = [word for word in text if word not in STOP_WORDS]
-                self.__content[name] = {'texto': text_sem_stop_words}
+            text = re.findall(r"[\w']+", text)  # (?<![0-9])
+            text = [i.lower() for i in text]
+            text_sem_stop_words = [word for word in text if word not in STOP_WORDS]
+            self.__content[name] = {'texto': text_sem_stop_words}
 
     def __unique_words(self):
         """
@@ -77,7 +79,11 @@ class Words:
                 self.__content['unique'].append(i)
 
     def __two_gram(self):
-        #self.__get_text()
+        """
+        Obter o vocabulário composto de grupos de duas palavras em sequência (2-gram).
+
+        :return:
+        """
         texto = [self.__content[key]['texto'] for key in self.__content
                                           if key != 'unique' and key != 'two_gram']
         two_gram = []
@@ -91,14 +97,22 @@ class Words:
                 self.__content['two_gram'].append(exp)
 
     def __count_unique(self):
-        #self.__unique_words()
+        """
+        Contar o número de vezes que uma palavra aparece em um texto, com base no vocabulário de
+        palavras únicas
+        :return:
+        """
         for name in self.__content:
             if name != 'unique' and name != 'two_gram':
                 add_element(self.__content[name], 'num_unique',
                             [self.__content[name]['texto'].count(word) for word in self.__content['unique']])
 
     def __count_two_gram(self):
-        #self.__two_gram()
+        """
+        Contar o número de vezes que uma palavra aparece em um texto, com base no vocabulário de
+        grupo de duas palavras
+        :return:
+        """
         for name in self.__content:
             if name != 'unique' and name != 'two_gram':
                 texto = [self.__content[name]['texto']]
@@ -113,6 +127,10 @@ class Words:
                             [text_gram.count(word) for word in self.__content['two_gram']])
 
     def __run(self):
+        """
+        Executar os métodos privados da classe Words
+        :return:
+        """
         if self.files:
             self.__get_text()
             self.__unique_words()
