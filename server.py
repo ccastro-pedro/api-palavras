4299b97c722797123313be66aa94ecaaff1ac532
ccastro-pedro <70175236+ccastro-pedro@users.noreply.github.com>
2020-10-26 09:40:01 -0300

    Add files via upload
    Novos comentários ao arquivo

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, flash, redirect, url_for
-from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
-from werkzeug.utils import secure_filename
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_restful import Resource, Api
 import classes
+
 
 # Inicializar a API do Flask:
 app = Flask(__name__)
@@ -12,30 +11,45 @@ words = classes.Words()
 
 
 class UploadFile(Resource):
+    """
+    Chamar o API para adicionar um novo arquivo (método add_files da classe Words)
+    """
     def put(self, file):
         words.add_files(file)
 
 api.add_resource(UploadFile, "/UploadFile/<string:file>")
 
 class UniqueVocab(Resource):
+    """
+    Chamar o API para visualizar o vocabulário de palavras únicas
+    """
     def get(self):
         return {'unique': words.unique_vocab}
 
 api.add_resource(UniqueVocab, "/UniqueVocab")
 
 class TwoGramVocab(Resource):
+    """
+    Chamar o API para visualizar o vocabulário de Two Gram
+    """
     def get(self):
         return {'TwoGram': words.two_gram_vocab}
 
 api.add_resource(TwoGramVocab, "/TwoGramVocab")
 
 class n_unique(Resource):
+    """
+    Chamar o API para visualizar os vetores de palavras únicas de cada texto
+    """
     def get(self):
         return words.num_unique
 
 api.add_resource(n_unique, "/NumberUnique")
 
 class n_TwoGram(Resource):
+    """
+    Chamar o API para visualizar os vetores de Two Gram de cada texto
+    """
     def get(self):
         return words.num_two_gram
 
